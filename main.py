eba3981efaac88447e66414434022aedd9576116
Robin Tregaskis <lokidokicoki@gmail.com>
2021-03-07 22:28:12 +0000

    test tkinter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import datetime
+import tkinter as tk
 
 conn = sqlite3.connect("jalopy.db")
 cursor = None
@@ -74,3 +75,28 @@ def createDB():
 createDB()
 conn.close();
 
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self,text="Qit", fg="red",
+                command=self.master.destroy)
+        
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi yall")
+
+root = tk.Tk()
+app = Application(master = root)
+app.mainloop()
