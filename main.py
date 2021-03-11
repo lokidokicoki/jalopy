44be9612e6d4649fafddf96d1349cd809efc2982
lokidokicoki <lokidokicoki@gmail.com>
2021-03-11 18:23:12 +0000

    feat(main): linter fixes

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sqlite3
-import datetime
-from collections import defaultdict
 from PyInquirer import prompt, Separator
 import configparser
 from shutil import copyfile
@@ -19,23 +16,26 @@ litresPerGallon = 4.54609
 kmPerMile = 1.60934
 mpgToL100km = 235.215
 
+
 def getFuelTypes(answers):
     """
     Get fuel types
     """
     types = db.getFuelTypes()
-    choices = [{'name':i[1],'value':i[0]} for i in types]
+    choices = [{'name': i[1], 'value':i[0]} for i in types]
 
-    return choices 
+    return choices
+
 
 def getRecordTypes(answers):
     """
     Get record types
     """
     types = db.getRecordTypes()
-    choices = [{'name':i[1],'value':i[0]} for i in types]
+    choices = [{'name': i[1], 'value':i[0]} for i in types]
 
-    return choices 
+    return choices
+
 
 def calculateEconomy(record, silent=False):
     """
@@ -54,7 +54,8 @@ def calculateEconomy(record, silent=False):
         print('{:0.2f} kpl'.format(kpl))
         print('{:0.2f} l/100Km'.format(l100))
 
-    return {'mpg':mpg, 'kpl':kpl, 'l100':l100}
+    return {'mpg': mpg, 'kpl': kpl, 'l100': l100}
+
 
 def createEditVehicle(vehicle=None):
     """
@@ -62,102 +63,102 @@ def createEditVehicle(vehicle=None):
     """
     questions = [
         {
-            'type':'input',
-            'name':'REG_NO',
-            'message':'Reg. No.:',
+            'type': 'input',
+            'name': 'REG_NO',
+            'message': 'Reg. No.:',
             'default': vehicle['REG_NO'] if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'MAKE',
-            'message':'Make',
+            'type': 'input',
+            'name': 'MAKE',
+            'message': 'Make',
             'default': vehicle['MAKE'] if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'MODEL',
-            'message':'Model',
+            'type': 'input',
+            'name': 'MODEL',
+            'message': 'Model',
             'default': vehicle['MODEL'] if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'YEAR',
-            'message':'Year',
+            'type': 'input',
+            'name': 'YEAR',
+            'message': 'Year',
             'default': str(vehicle['YEAR']) if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'PURCHASE_DATE',
-            'message':'Purchase Date',
+            'type': 'input',
+            'name': 'PURCHASE_DATE',
+            'message': 'Purchase Date',
             'default': vehicle['PURCHASE_DATE'] if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'PURCHASE_PRICE',
-            'message':'Purchase Price',
+            'type': 'input',
+            'name': 'PURCHASE_PRICE',
+            'message': 'Purchase Price',
             'default': str(vehicle['PURCHASE_PRICE']) if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'list',
-            'name':'FUEL_TYPE_ID',
-            'message':'Fuel type',
-            'choices':getFuelTypes,
+            'type': 'list',
+            'name': 'FUEL_TYPE_ID',
+            'message': 'Fuel type',
+            'choices': getFuelTypes,
             'default': vehicle['FUEL_TYPE_ID'] if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'FUEL_CAPACITY',
-            'message':'Fuel Capacity (ltr)',
+            'type': 'input',
+            'name': 'FUEL_CAPACITY',
+            'message': 'Fuel Capacity (ltr)',
             'default': str(vehicle['FUEL_CAPACITY']) if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'OIL_TYPE',
-            'message':'Oil Type',
+            'type': 'input',
+            'name': 'OIL_TYPE',
+            'message': 'Oil Type',
             'default': vehicle['OIL_TYPE'] if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'OIL_CAPACITY',
-            'message':'Oil Capacity (ltr)',
+            'type': 'input',
+            'name': 'OIL_CAPACITY',
+            'message': 'Oil Capacity (ltr)',
             'default': str(vehicle['OIL_CAPACITY']) if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'TYRE_SIZE_FRONT',
-            'message':'Tyre size front',
+            'type': 'input',
+            'name': 'TYRE_SIZE_FRONT',
+            'message': 'Tyre size front',
             'default': vehicle['TYRE_SIZE_FRONT'] if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'TYRE_PRESSURE_FRONT',
-            'message':'Tyre pressure front',
+            'type': 'input',
+            'name': 'TYRE_PRESSURE_FRONT',
+            'message': 'Tyre pressure front',
             'default': str(vehicle['TYRE_PRESSURE_FRONT']) if vehicle else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'TYRE_SIZE_REAR',
-            'message':'Tyre size rear',
-            'default': lambda answers: vehicle['TYRE_SIZE_REAR'] if vehicle else answers['TYRE_SIZE_FRONT'],
+            'type': 'input',
+            'name': 'TYRE_SIZE_REAR',
+            'message': 'Tyre size rear',
+            'default': lambda x: vehicle['TYRE_SIZE_REAR'] if vehicle else x['TYRE_SIZE_FRONT'],
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'TYRE_PRESSURE_REAR',
-            'message':'Tyre pressure rear',
-            'default': lambda answers: str(vehicle['TYRE_PRESSURE_REAR']) if vehicle else answers['TYRE_PRESSURE_FRONT'],
+            'type': 'input',
+            'name': 'TYRE_PRESSURE_REAR',
+            'message': 'Tyre pressure rear',
+            'default': lambda x: str(vehicle['TYRE_PRESSURE_REAR']) if vehicle else x['TYRE_PRESSURE_FRONT'],
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
     ]
@@ -181,6 +182,7 @@ def createEditVehicle(vehicle=None):
 
     db.addVehicle(answers)
 
+
 def createEditRecord(record=None):
     """
     Create/edit 'record'
@@ -188,63 +190,62 @@ def createEditRecord(record=None):
     allVehicles = db.loadVehicles()
     questions = [
         {
-            'type':'list',
-            'name':'VEHICLE_ID',
-            'message':'Reg. No.:',
+            'type': 'list',
+            'name': 'VEHICLE_ID',
+            'message': 'Reg. No.:',
             'default': record['VEHICLE_ID'] if record else '',
-            'choices':[{'name':i[1],'value':i[0]} for i in allVehicles]
+            'choices': [{'name': i[1], 'value': i[0]} for i in allVehicles]
         },
         {
-            'type':'list',
-            'name':'RECORD_TYPE_ID',
-            'message':'Type',
+            'type': 'list',
+            'name': 'RECORD_TYPE_ID',
+            'message': 'Type',
             'default': record['RECORD_TYPE_ID'] if record else '',
-            'choices':getRecordTypes
+            'choices': getRecordTypes
         },
         {
-            'type':'input',
-            'name':'DATE',
-            'message':'Date',
+            'type': 'input',
+            'name': 'DATE',
+            'message': 'Date',
             'default': record['DATE'] if record else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'ODOMETER',
-            'message':'Odometer',
+            'type': 'input',
+            'name': 'ODOMETER',
+            'message': 'Odometer',
             'default': str(record['ODOMETER']) if record else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'TRIP',
-            'message':'Trip (optional)',
+            'type': 'input',
+            'name': 'TRIP',
+            'message': 'Trip (optional)',
             'default': str(record['TRIP']) if record else '',
         },
         {
-            'type':'input',
-            'name':'COST',
-            'message':'Cost',
+            'type': 'input',
+            'name': 'COST',
+            'message': 'Cost',
             'default': str(record['COST']) if record else '',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'ITEM_COUNT',
-            'message':'Item Count',
+            'type': 'input',
+            'name': 'ITEM_COUNT',
+            'message': 'Item Count',
             'default': str(record['ITEM_COUNT']) if record else '1',
             'validate': lambda val: len(val) != 0 or 'Please supply a value'
         },
         {
-            'type':'input',
-            'name':'NOTES',
-            'message':'Notes (optional)',
+            'type': 'input',
+            'name': 'NOTES',
+            'message': 'Notes (optional)',
             'default': record['NOTES'] if record else '',
         },
     ]
 
     answers = prompt(questions)
-
 
     # process, then saveo
     if(record):
@@ -263,6 +264,7 @@ def createEditRecord(record=None):
 
     db.addRecord(answers)
 
+
 def selectVehicle():
     """
     Prompt user to select a vehicle
@@ -271,16 +273,17 @@ def selectVehicle():
 
     questions = [
         {
-            'type':'list',
-            'name':'opts',
-            'message':'Select vehicle',
-            'choices':[{'name':i[1],'value':i[0]} for i in allVehicles]
+            'type': 'list',
+            'name': 'opts',
+            'message': 'Select vehicle',
+            'choices': [{'name': i[1], 'value': i[0]} for i in allVehicles]
         }
     ]
 
     answers = prompt(questions)
 
     return next(x for x in allVehicles if x[0] == answers['opts'])
+
 
 def selectRecord(vehicle):
     """
@@ -290,16 +293,17 @@ def selectRecord(vehicle):
 
     questions = [
         {
-            'type':'list',
-            'name':'opts',
-            'message':'Select record',
-            'choices':[{'name':i['DATE'],'value':i['ID']} for i in allRecords]
+            'type': 'list',
+            'name': 'opts',
+            'message': 'Select record',
+            'choices': [{'name': i['DATE'], 'value': i['ID']} for i in allRecords]
         }
     ]
 
     answers = prompt(questions)
 
     return next(x for x in allRecords if x[0] == answers['opts'])
+
 
 def stats(vehicle):
     """
@@ -313,7 +317,7 @@ def stats(vehicle):
     avgMpg = 0
     avgKpl = 0
     avgL100 = 0
-    counts = [ {'id':x['ID'],'name':x['NAME'], 'count':0} for x in types ]
+    counts = [{'id': x['ID'], 'name':x['NAME'], 'count':0} for x in types]
 
     for record in records:
         count = next(x for x in counts if x['id'] == int(record['RECORD_TYPE_ID']))
@@ -321,9 +325,9 @@ def stats(vehicle):
         if record['RECORD_TYPE_ID'] == '1':
             eff = calculateEconomy(record, True)
 
-            avgMpg= avgMpg + eff['mpg']
-            avgKpl= avgKpl + eff['kpl']
-            avgL100= avgL100 + eff['l100']
+            avgMpg = avgMpg + eff['mpg']
+            avgKpl = avgKpl + eff['kpl']
+            avgL100 = avgL100 + eff['l100']
 
         totalCost = totalCost + record['COST']
     fuelCount = next(x for x in counts if x['id'] == 1)
@@ -337,32 +341,33 @@ def stats(vehicle):
     for i in counts:
         print('{}: {}'.format(i['name'], i['count']))
 
+
 def recordsMenu():
     """
     CRUD ops for records
     """
     questions = [
         {
-            'type':'list',
-            'name':'opts',
-            'message':'Records menu',
-            'choices':[
+            'type': 'list',
+            'name': 'opts',
+            'message': 'Records menu',
+            'choices': [
                 {
-                    'name':'Add',
-                    'value':'add'
+                    'name': 'Add',
+                    'value': 'add'
                 },
                 {
-                    'name':'Edit',
-                    'value':'edit'
+                    'name': 'Edit',
+                    'value': 'edit'
                 },
                 {
-                    'name':'Remove',
-                    'value':'remove'
+                    'name': 'Remove',
+                    'value': 'remove'
                 },
                 Separator(),
                 {
-                    'name':'Back',
-                    'value':'back'
+                    'name': 'Back',
+                    'value': 'back'
                 },
             ]
         }
@@ -383,37 +388,38 @@ def recordsMenu():
         print('remove vehicle')
     else:
         print('return to main')
-    
+
+
 def vehiclesMenu():
     """
     CRUD ops for vehicles
     """
     questions = [
         {
-            'type':'list',
-            'name':'opts',
-            'message':'Vehicle menu',
-            'choices':[
+            'type': 'list',
+            'name': 'opts',
+            'message': 'Vehicle menu',
+            'choices': [
                 {
-                    'name':'Add',
-                    'value':'add'
+                    'name': 'Add',
+                    'value': 'add'
                 },
                 {
-                    'name':'Edit',
-                    'value':'edit'
+                    'name': 'Edit',
+                    'value': 'edit'
                 },
                 {
-                    'name':'Remove',
-                    'value':'remove'
+                    'name': 'Remove',
+                    'value': 'remove'
                 },
                 {
-                    'name':'Stats',
-                    'value':'stats'
+                    'name': 'Stats',
+                    'value': 'stats'
                 },
                 Separator(),
                 {
-                    'name':'Back',
-                    'value':'back'
+                    'name': 'Back',
+                    'value': 'back'
                 },
             ]
         }
@@ -436,7 +442,7 @@ def vehiclesMenu():
         print('remove vehicle')
     else:
         print('return to main')
-    
+
 
 def mainMenu():
     """
@@ -446,32 +452,31 @@ def mainMenu():
 
     questions = [
         {
-            'type':'list',
-            'name':'opts',
-            'message':'Things to do',
-            'choices':[
+            'type': 'list',
+            'name': 'opts',
+            'message': 'Things to do',
+            'choices': [
                 {
-                    'name':'Vehicles',
-                    'key':'v',
-                    'value':'vehicles'
+                    'name': 'Vehicles',
+                    'key': 'v',
+                    'value': 'vehicles'
                 },
                 {
-                    'name':'Records',
-                    'key':'r',
-                    'value':'records'
+                    'name': 'Records',
+                    'key': 'r',
+                    'value': 'records'
                 },
                 Separator(),
                 {
                     'name': 'Exit',
-                    'key':'x',
-                    'value':'exit'
+                    'key': 'x',
+                    'value': 'exit'
                 }
             ]
         }
-    ]   
+    ]
 
     answers = prompt(questions)
-
 
     if answers['opts'] == 'exit':
         running = False
@@ -481,6 +486,7 @@ def mainMenu():
     elif answers["opts"] == 'records':
         print('mod records')
         recordsMenu()
+
 
 def main():
     if exists(config['db']['path']):
@@ -496,5 +502,6 @@ def main():
     db.conn.close()
     copyfile('jalopy.db', config['db']['path'])
     print('Night night')
+
 
 main()
