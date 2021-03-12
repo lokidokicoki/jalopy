7bb84061cfe88648fd82ecac7e0570e7c81db1c4
lokidokicoki <lokidokicoki@gmail.com>
2021-03-12 11:51:18 +0000

    feat(app): stub gtk

---
+++ app.py
@@ -0,0 +1,22 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class MyWin(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello")
+
+        self.button = Gtk.Button(label="clickty")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
+
+    def on_button_clicked(self, widget):
+        print("boop")
+
+
+win = MyWin()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
