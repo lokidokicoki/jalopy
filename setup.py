cc7e5f2b7bff6eeaed7c848cc6a974718dfa0617
lokidokicoki <lokidokicoki@gmail.com>
2021-03-12 16:27:28 +0000

    feat(setup.py): a setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="Jalo.py",
+    version="1.0",
+    author="lokidokicoki",
+    author_email="lokidokicoki@gmail.com",
+    url="https://github.com/lokidokicoki/jalo.py",
+)
