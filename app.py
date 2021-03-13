529f786490e2d81ecca42f0f11d135f0c7b5e8df
lokidokicoki <lokidokicoki@gmail.com>
2021-03-13 15:51:43 +0000

    feat(app): s/MyWin/MainWindow

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 
-class MyWin(Gtk.Window):
+class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Hello")
 
@@ -16,7 +16,7 @@ class MyWin(Gtk.Window):
         print("boop")
 
 
-win = MyWin()
+win = MainWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
