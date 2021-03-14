8d09e37152aad4588d04469a836b82ccbb36d533
lokidokicoki <lokidokicoki@gmail.com>
2021-03-14 12:03:02 +0000

    feat(main): cache parsed args. check for `no backup`

--- main.py
+++ main.py
@@ -7,36 +7,46 @@ import cli
 import app
 
 DEBUG = False
+args = None
 
 
-def main(mode):
+def main():
     config = configparser.ConfigParser()
     config.read("config.ini")
-    if exists(config["db"]["path"]):
-        print("Shared DB exists")
-        copyfile(config["db"]["path"], "jalopy.db")
+
+    if args.no_backup is False:
+        if exists(config["db"]["path"]):
+            print("Shared DB exists")
+            copyfile(config["db"]["path"], "jalopy.db")
+        else:
+            print("Shared DB not found, we will fix it!")
     else:
-        print("Shared DB not found, we will fix it!")
+        print("Not using backup")
+
+    db.init()
     db.createDB()
 
     # if cli
-    if mode == "cli":
+    if args.mode == "cli":
         cli.main()
-    elif mode == "gui":
+    elif args.mode == "gui":
         app.main()
     else:
         print("Unknown mode, exiting")
 
     db.conn.close()
-    copyfile("jalopy.db", config["db"]["path"])
+
+    if args.no_backup is False:
+        copyfile("jalopy.db", config["db"]["path"])
     print("Night night")
 
 
 parser = argparse.ArgumentParser(description="Jalo.py")
 parser.add_argument("-m", "--mode", default="cli", help="use gui")
 parser.add_argument("-d", "--debug", help="use debug", action="store_true")
+parser.add_argument("-n", "--no-backup", help="do not use backup", action="store_true")
 args = parser.parse_args()
 
 DEBUG = args.debug
 
-main(args.mode)
+main()
