e0a3c00655407ae983b98a8a0cc398405e15b40b
lokidokicoki <lokidokicoki@gmail.com>
2021-03-29 17:30:27 +0100

    split vehicles, records into separate classes

---
+++ db/vehicles.py
@@ -0,0 +1,93 @@
+class Vehicles:
+    def __init__(self, conn, cursor):
+        self.cursor = cursor
+        self.conn = conn
+
+    def getAll(self):
+        self.cursor.execute(
+            """
+            SELECT
+            ID,
+            REG_NO,
+            MAKE,
+            MODEL,
+            YEAR,
+            PURCHASE_PRICE,
+            PURCHASE_DATE,
+            PURCHASE_ODOMETER,
+            FUEL_TYPE_ID,
+            FUEL_CAPACITY,
+            OIL_TYPE,
+            OIL_CAPACITY,
+            TYRE_SIZE_FRONT,
+            TYRE_SIZE_REAR,
+            TYRE_PRESSURE_FRONT,
+            TYRE_PRESSURE_REAR
+            FROM VEHICLES
+            """
+        )
+
+        return self.cursor.fetchall()
+
+    def addVehicle(self, vehicle):
+        """
+        Add/amend vehicle record
+        """
+        if "ID" in vehicle:
+            sql = """
+            UPDATE VEHICLES SET
+            REG_NO=:REG_NO,
+            MAKE=:MAKE,
+            MODEL=:MODEL,
+            YEAR=:YEAR,
+            PURCHASE_PRICE=:PURCHASE_PRICE,
+            PURCHASE_DATE=:PURCHASE_DATE,
+            PURCHASE_ODOMETER=:PURCHASE_ODOMETER,
+            FUEL_TYPE_ID=:FUEL_TYPE_ID,
+            FUEL_CAPACITY=:FUEL_CAPACITY,
+            OIL_TYPE=:OIL_TYPE,
+            OIL_CAPACITY=:OIL_CAPACITY,
+            TYRE_SIZE_FRONT=:TYRE_SIZE_FRONT,
+            TYRE_SIZE_REAR=:TYRE_SIZE_REAR,
+            TYRE_PRESSURE_FRONT=:TYRE_PRESSURE_FRONT,
+            TYRE_PRESSURE_REAR=:TYRE_PRESSURE_REAR
+            WHERE ID=:ID
+            """
+
+        else:
+            sql = """INSERT INTO VEHICLES (
+                REG_NO,
+                MAKE,
+                MODEL,
+                YEAR,
+                PURCHASE_PRICE,
+                PURCHASE_DATE,
+                PURCHASE_ODOMETER,
+                FUEL_TYPE_ID,
+                FUEL_CAPACITY,
+                OIL_TYPE,
+                OIL_CAPACITY,
+                TYRE_SIZE_FRONT,
+                TYRE_SIZE_REAR,
+                TYRE_PRESSURE_FRONT,
+                TYRE_PRESSURE_REAR
+                ) VALUES (
+                :REG_NO,
+                :MAKE,
+                :MODEL,
+                :YEAR,
+                :PURCHASE_PRICE,
+                :PURCHASE_DATE,
+                :PURCHASE_ODOMETER,
+                :FUEL_TYPE_ID,
+                :FUEL_CAPACITY,
+                :OIL_TYPE,
+                :OIL_CAPACITY,
+                :TYRE_SIZE_FRONT,
+                :TYRE_SIZE_REAR,
+                :TYRE_PRESSURE_FRONT,
+                :TYRE_PRESSURE_REAR
+                )"""
+
+        self.cursor.execute(sql, vehicle)
+        self.conn.commit()
