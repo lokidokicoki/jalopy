56aca437c029d76f3c7e0b79a17afb5804477171
lokidokicoki <lokidokicoki@gmail.com>
2021-03-29 18:16:42 +0100

    split out records, update dbclient

--- db/dbclient.py
+++ db/dbclient.py
@@ -1,148 +1,104 @@
 import sqlite3
 from . import vehicles
+from . import records
 
 
-class DBClient:
-    def __init__(self):
-        self.conn = sqlite3.connect("jalopy.db")
-        self.conn.row_factory = sqlite3.Row
-        self.cursor = self.conn.cursor()
-        self.vehicles = vehicles.Vehicles(self.conn, self.cursor)
+conn = None
+cursor = None
 
-    def createDatabase(self):
-        """
-        Create DB if none exists
-        """
-        sql = "SELECT name from sqlite_master WHERE type='table' and name=?"
 
-        # test for vehicles vehicle
-        self.cursor.execute(sql, ["VEHICLES"])
-        result = self.cursor.fetchone()
+def init():
+    global conn, cursor
+    conn = sqlite3.connect("jalopy.db")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    vehicles.init(conn, cursor)
+    records.init(conn, cursor)
 
-        if result is None:
-            print("create jalopy.db tables")
-            self.cursor.executescript(
-                """
-                CREATE TABLE VEHICLES(
-                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                    REG_NO CHAR(10) NOT NULL,
-                    MAKE TEXT NOT NULL,
-                    MODEL TEXT NOT NULL,
-                    YEAR INTEGER NOT NULL,
-                    PURCHASE_DATE TEXT,
-                    PURCHASE_PRICE REAL,
-                    PURCHASE_ODOMETER INTEGER,
-                    FUEL_TYPE_ID INTEGER NOT NULL,
-                    FUEL_CAPACITY REAL,
-                    OIL_TYPE TEXT,
-                    OIL_CAPACITY REAL,
-                    TYRE_SIZE_FRONT TEXT,
-                    TYRE_SIZE_REAR TEXT,
-                    TYRE_PRESSURE_FRONT REAL,
-                    TYRE_PRESSURE_REAR REAL
-                );
 
-                CREATE TABLE FUEL_TYPES(
-                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                    NAME TEXT NOT NULL
-                );
+def createDatabase():
+    """
+    Create DB if none exists
+    """
+    sql = "SELECT name from sqlite_master WHERE type='table' and name=?"
 
-                INSERT INTO FUEL_TYPES(NAME) VALUES('Unleaded');
-                INSERT INTO FUEL_TYPES(NAME) VALUES('Super Unleaded');
-                INSERT INTO FUEL_TYPES(NAME) VALUES('Diesel');
-                INSERT INTO FUEL_TYPES(NAME) VALUES('Super Diesel');
+    # test for vehicles vehicle
+    cursor.execute(sql, ["VEHICLES"])
+    result = cursor.fetchone()
 
-                CREATE TABLE RECORDS(
-                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                    VEHICLE_ID INTEGER NOT NULL,
-                    RECORD_TYPE_ID INTEGER NOT NULL,
-                    DATE TEXT NOT NULL,
-                    ODOMETER INTEGER NOT NULL,
-                    TRIP REAL,
-                    COST REAL NOT NULL,
-                    ITEM_COUNT REAL,
-                    NOTES TEXT
-                );
-
-                CREATE TABLE RECORD_TYPES(
-                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                    NAME TEXT NOT NULL
-                );
-                INSERT INTO RECORD_TYPES(NAME) VALUES('Fuel');
-                INSERT INTO RECORD_TYPES(NAME) VALUES('Service');
-                INSERT INTO RECORD_TYPES(NAME) VALUES('Maintenance');
-                INSERT INTO RECORD_TYPES(NAME) VALUES('Tax');
-                INSERT INTO RECORD_TYPES(NAME) VALUES('Insurance');
-                INSERT INTO RECORD_TYPES(NAME) VALUES('M.O.T.');
-                """
-            )
-            self.conn.commit()
-        else:
-            print("DB exists")
-
-    def getFuelTypes(self):
-        """
-        Get fuel types
-        """
-        self.cursor.execute("SELECT ID,NAME from FUEL_TYPES")
-        return self.cursor.fetchall()
-
-    def getRecordTypes(self):
-        """
-        Get all record types
-        """
-        self.cursor.execute("SELECT ID,NAME from RECORD_TYPES")
-        return self.cursor.fetchall()
-
-    def getRecords(self, vehicleId=None):
-        sql = """
-        select * from RECORDS
-        """
-
-        if vehicleId:
-            sql = sql + " where VEHICLE_ID=:VEHICLE_ID"
-
-        self.cursor.execute(sql, {"VEHICLE_ID": vehicleId})
-
-        return self.cursor.fetchall()
-
-    def addRecord(self, record):
-        """
-        Add/amend record
-        """
-        if "ID" in record:
-            sql = """
-                UPDATE RECORDS SET
-                VEHICLE_ID=:VEHICLE_ID,
-                RECORD_TYPE_ID=:RECORD_TYPE_ID,
-                DATE=:DATE,
-                ODOMETER=:ODOMETER,
-                TRIP=:TRIP,
-                COST=:COST,
-                ITEM_COUNT=:ITEM_COUNT,
-                NOTES=:NOTES
-            WHERE ID=:ID
+    if result is None:
+        print("create jalopy.db tables")
+        cursor.executescript(
             """
-        else:
-            sql = """INSERT INTO RECORDS (
-                    VEHICLE_ID,
-                    RECORD_TYPE_ID,
-                    DATE,
-                    ODOMETER,
-                    TRIP,
-                    COST,
-                    ITEM_COUNT,
-                    NOTES
-        ) VALUES (
-                    :VEHICLE_ID,
-                    :RECORD_TYPE_ID,
-                    :DATE,
-                    :ODOMETER,
-                    :TRIP,
-                    :COST,
-                    :ITEM_COUNT,
-                    :NOTES
-        )"""
+            CREATE TABLE VEHICLES(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                REG_NO CHAR(10) NOT NULL,
+                MAKE TEXT NOT NULL,
+                MODEL TEXT NOT NULL,
+                YEAR INTEGER NOT NULL,
+                PURCHASE_DATE TEXT,
+                PURCHASE_PRICE REAL,
+                PURCHASE_ODOMETER INTEGER,
+                FUEL_TYPE_ID INTEGER NOT NULL,
+                FUEL_CAPACITY REAL,
+                OIL_TYPE TEXT,
+                OIL_CAPACITY REAL,
+                TYRE_SIZE_FRONT TEXT,
+                TYRE_SIZE_REAR TEXT,
+                TYRE_PRESSURE_FRONT REAL,
+                TYRE_PRESSURE_REAR REAL
+            );
 
-        self.cursor.execute(sql, record)
-        self.conn.commit()
+            CREATE TABLE FUEL_TYPES(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                NAME TEXT NOT NULL
+            );
+
+            INSERT INTO FUEL_TYPES(NAME) VALUES('Unleaded');
+            INSERT INTO FUEL_TYPES(NAME) VALUES('Super Unleaded');
+            INSERT INTO FUEL_TYPES(NAME) VALUES('Diesel');
+            INSERT INTO FUEL_TYPES(NAME) VALUES('Super Diesel');
+
+            CREATE TABLE RECORDS(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                VEHICLE_ID INTEGER NOT NULL,
+                RECORD_TYPE_ID INTEGER NOT NULL,
+                DATE TEXT NOT NULL,
+                ODOMETER INTEGER NOT NULL,
+                TRIP REAL,
+                COST REAL NOT NULL,
+                ITEM_COUNT REAL,
+                NOTES TEXT
+            );
+
+            CREATE TABLE RECORD_TYPES(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                NAME TEXT NOT NULL
+            );
+            INSERT INTO RECORD_TYPES(NAME) VALUES('Fuel');
+            INSERT INTO RECORD_TYPES(NAME) VALUES('Service');
+            INSERT INTO RECORD_TYPES(NAME) VALUES('Maintenance');
+            INSERT INTO RECORD_TYPES(NAME) VALUES('Tax');
+            INSERT INTO RECORD_TYPES(NAME) VALUES('Insurance');
+            INSERT INTO RECORD_TYPES(NAME) VALUES('M.O.T.');
+            """
+        )
+        conn.commit()
+    else:
+        print("DB exists")
+
+
+def getRecordTypes():
+    """
+    Get all record types
+    """
+    cursor.execute("SELECT ID,NAME from RECORD_TYPES")
+    return cursor.fetchall()
+
+
+def getFuelTypes():
+    """
+    Get fuel types
+    """
+    cursor.execute("SELECT ID,NAME from FUEL_TYPES")
+    return cursor.fetchall()
--- db/vehicles.py
+++ db/vehicles.py
@@ -1,13 +1,67 @@
-class Vehicles:
-    def __init__(self, conn, cursor):
-        self.cursor = cursor
-        self.conn = conn
+cursor = None
+conn = None
 
-    def getAll(self):
-        self.cursor.execute(
-            """
-            SELECT
-            ID,
+
+def init(_conn, _cursor):
+    global cursor, conn
+    cursor = _cursor
+    conn = _conn
+
+
+def get():
+    cursor.execute(
+        """
+        SELECT
+        ID,
+        REG_NO,
+        MAKE,
+        MODEL,
+        YEAR,
+        PURCHASE_PRICE,
+        PURCHASE_DATE,
+        PURCHASE_ODOMETER,
+        FUEL_TYPE_ID,
+        FUEL_CAPACITY,
+        OIL_TYPE,
+        OIL_CAPACITY,
+        TYRE_SIZE_FRONT,
+        TYRE_SIZE_REAR,
+        TYRE_PRESSURE_FRONT,
+        TYRE_PRESSURE_REAR
+        FROM VEHICLES
+        """
+    )
+
+    return cursor.fetchall()
+
+
+def add(vehicle):
+    """
+    Add/amend vehicle record
+    """
+    if "ID" in vehicle:
+        sql = """
+        UPDATE VEHICLES SET
+        REG_NO=:REG_NO,
+        MAKE=:MAKE,
+        MODEL=:MODEL,
+        YEAR=:YEAR,
+        PURCHASE_PRICE=:PURCHASE_PRICE,
+        PURCHASE_DATE=:PURCHASE_DATE,
+        PURCHASE_ODOMETER=:PURCHASE_ODOMETER,
+        FUEL_TYPE_ID=:FUEL_TYPE_ID,
+        FUEL_CAPACITY=:FUEL_CAPACITY,
+        OIL_TYPE=:OIL_TYPE,
+        OIL_CAPACITY=:OIL_CAPACITY,
+        TYRE_SIZE_FRONT=:TYRE_SIZE_FRONT,
+        TYRE_SIZE_REAR=:TYRE_SIZE_REAR,
+        TYRE_PRESSURE_FRONT=:TYRE_PRESSURE_FRONT,
+        TYRE_PRESSURE_REAR=:TYRE_PRESSURE_REAR
+        WHERE ID=:ID
+        """
+
+    else:
+        sql = """INSERT INTO VEHICLES (
             REG_NO,
             MAKE,
             MODEL,
@@ -23,71 +77,23 @@ class Vehicles:
             TYRE_SIZE_REAR,
             TYRE_PRESSURE_FRONT,
             TYRE_PRESSURE_REAR
-            FROM VEHICLES
-            """
-        )
+            ) VALUES (
+            :REG_NO,
+            :MAKE,
+            :MODEL,
+            :YEAR,
+            :PURCHASE_PRICE,
+            :PURCHASE_DATE,
+            :PURCHASE_ODOMETER,
+            :FUEL_TYPE_ID,
+            :FUEL_CAPACITY,
+            :OIL_TYPE,
+            :OIL_CAPACITY,
+            :TYRE_SIZE_FRONT,
+            :TYRE_SIZE_REAR,
+            :TYRE_PRESSURE_FRONT,
+            :TYRE_PRESSURE_REAR
+            )"""
 
-        return self.cursor.fetchall()
-
-    def addVehicle(self, vehicle):
-        """
-        Add/amend vehicle record
-        """
-        if "ID" in vehicle:
-            sql = """
-            UPDATE VEHICLES SET
-            REG_NO=:REG_NO,
-            MAKE=:MAKE,
-            MODEL=:MODEL,
-            YEAR=:YEAR,
-            PURCHASE_PRICE=:PURCHASE_PRICE,
-            PURCHASE_DATE=:PURCHASE_DATE,
-            PURCHASE_ODOMETER=:PURCHASE_ODOMETER,
-            FUEL_TYPE_ID=:FUEL_TYPE_ID,
-            FUEL_CAPACITY=:FUEL_CAPACITY,
-            OIL_TYPE=:OIL_TYPE,
-            OIL_CAPACITY=:OIL_CAPACITY,
-            TYRE_SIZE_FRONT=:TYRE_SIZE_FRONT,
-            TYRE_SIZE_REAR=:TYRE_SIZE_REAR,
-            TYRE_PRESSURE_FRONT=:TYRE_PRESSURE_FRONT,
-            TYRE_PRESSURE_REAR=:TYRE_PRESSURE_REAR
-            WHERE ID=:ID
-            """
-
-        else:
-            sql = """INSERT INTO VEHICLES (
-                REG_NO,
-                MAKE,
-                MODEL,
-                YEAR,
-                PURCHASE_PRICE,
-                PURCHASE_DATE,
-                PURCHASE_ODOMETER,
-                FUEL_TYPE_ID,
-                FUEL_CAPACITY,
-                OIL_TYPE,
-                OIL_CAPACITY,
-                TYRE_SIZE_FRONT,
-                TYRE_SIZE_REAR,
-                TYRE_PRESSURE_FRONT,
-                TYRE_PRESSURE_REAR
-                ) VALUES (
-                :REG_NO,
-                :MAKE,
-                :MODEL,
-                :YEAR,
-                :PURCHASE_PRICE,
-                :PURCHASE_DATE,
-                :PURCHASE_ODOMETER,
-                :FUEL_TYPE_ID,
-                :FUEL_CAPACITY,
-                :OIL_TYPE,
-                :OIL_CAPACITY,
-                :TYRE_SIZE_FRONT,
-                :TYRE_SIZE_REAR,
-                :TYRE_PRESSURE_FRONT,
-                :TYRE_PRESSURE_REAR
-                )"""
-
-        self.cursor.execute(sql, vehicle)
-        self.conn.commit()
+    cursor.execute(sql, vehicle)
+    conn.commit()
