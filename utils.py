830012adfb61d13e8cbb0da2231c62dd7d67ba89
lokidokicoki <lokidokicoki@gmail.com>
2021-07-22 23:45:47 +0100

    feat: utils: convert to class, update for new entity manager methods

--- utils.py
+++ utils.py
@@ -1,70 +1,67 @@
 """
 Collection of utility functions
 """
-from db.dbclient import DatabaseClient
+from entities.entity_manager import EntityManager, EntityType
 
 LITRES_PER_GALLON = 4.54609
 KM_PER_MILE = 1.60934
 MPG_TO_L100_KM = 235.215
 
-dbclient = None
 
+class Utils:
+    def __init__(self, entity_manager: EntityManager):
+        self.entity_manager = entity_manager
 
-def set_dbclient(client: DatabaseClient):
-    dbclient = client
+    @staticmethod
+    def calculate_economy(record):
+        """
+        mpg
+        l/100km
+        km/l
+        """
+        trip_km = record.trip * KM_PER_MILE
+        kpl = trip_km / record.item_count
+        mpg = record.trip / (record.item_count / LITRES_PER_GALLON)
+        l100 = MPG_TO_L100_KM / mpg
 
+        return {"mpg": mpg, "kpl": kpl, "l100": l100}
 
-def calculate_economy(record):
-    """
-    mpg
-    l/100km
-    km/l
-    """
-    trip_km = record["TRIP"] * KM_PER_MILE
-    kpl = trip_km / record["ITEM_COUNT"]
-    mpg = record["TRIP"] / (record["ITEM_COUNT"] / LITRES_PER_GALLON)
-    l100 = MPG_TO_L100_KM / mpg
+    def stats(self, vehicle):
+        """
+        Accumulative stats for this vehicle
+        """
+        # get all records for this vehicle
+        records = self.entity_manager.get_records_for_vehicle(vehicle.entity_id)
+        types = self.entity_manager.record_types
+        total_cost = 0
+        avg_mpg = 0
+        avg_km_per_litre = 0
+        avg_l100 = 0
+        counts = [{"id": x.entity_id, "name": x.name, "count": 0} for x in types]
 
-    return {"mpg": mpg, "kpl": kpl, "l100": l100}
+        for record in records:
+            print(record)
+            count = next(x for x in counts if x["id"] == record.record_type_id)
+            count["count"] = count["count"] + 1
+            if record.record_type_id == 1:
+                eff = self.calculate_economy(record)
 
+                avg_mpg = avg_mpg + eff["mpg"]
+                avg_km_per_litre = avg_km_per_litre + eff["kpl"]
+                avg_l100 = avg_l100 + eff["l100"]
 
-def stats(vehicle):
-    """
-    Accumulative stats for this vehicle
-    """
-    global dbclient
+            total_cost = total_cost + record.cost
+        fuel_count = next(x for x in counts if x["id"] == 1)
 
-    # get all records for this vehicle
-    records = dbclient.records.get(vehicle["ID"])
-    types = dbclient.get_record_types()
-    total_cost = 0
-    avg_mpg = 0
-    avg_km_per_litre = 0
-    avg_l100 = 0
-    counts = [{"id": x["ID"], "name": x["NAME"], "count": 0} for x in types]
+        if fuel_count["count"] > 0:
+            avg_mpg = avg_mpg / fuel_count["count"]
+            avg_km_per_litre = avg_km_per_litre / fuel_count["count"]
+            avg_l100 = avg_l100 / fuel_count["count"]
 
-    for record in records:
-        count = next(x for x in counts if x["id"] == record["RECORD_TYPE_ID"])
-        count["count"] = count["count"] + 1
-        if record["RECORD_TYPE_ID"] == 1:
-            eff = calculate_economy(record)
-
-            avg_mpg = avg_mpg + eff["mpg"]
-            avg_km_per_litre = avg_km_per_litre + eff["kpl"]
-            avg_l100 = avg_l100 + eff["l100"]
-
-        total_cost = total_cost + record["COST"]
-    fuel_count = next(x for x in counts if x["id"] == 1)
-
-    if fuel_count["count"] > 0:
-        avg_mpg = avg_mpg / fuel_count["count"]
-        avg_km_per_litre = avg_km_per_litre / fuel_count["count"]
-        avg_l100 = avg_l100 / fuel_count["count"]
-
-    return {
-        "counts": counts,
-        "avg_mpg": avg_mpg,
-        "avg_km_per_litre": avg_km_per_litre,
-        "avg_l100": avg_l100,
-        "total_cost": total_cost,
-    }
+        return {
+            "counts": counts,
+            "avg_mpg": avg_mpg,
+            "avg_km_per_litre": avg_km_per_litre,
+            "avg_l100": avg_l100,
+            "total_cost": total_cost,
+        }
